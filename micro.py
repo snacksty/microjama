19368359bda8df33b9ba322a07e7aee7a8a3307b
snackspixel <juane.martinez15@est.unanleon.edu.ni>
2023-06-06 12:24:32 +0200

    Start project

---
+++ micro.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as gpio
+import MFRC522 as mfrc
+
+def setup():
+    pass
+
+def loop():
+    pass
+
+def destroy():
+    pass
+
+if __name__ = '__main__':
+    try:
+        pass
+    except:
+        pass
+    finally:
+        pass
